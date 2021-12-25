4cbb57cc239146ffa0906e2134da09fa1388436e
Daniel Luo <dyl24@case.edu>
2021-12-25 16:20:53 -0500

    Reset the project to use SwiftUI instead of Storyboard

---
+++ AutoVolume/AutoVolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoVolumeApp.swift
+//  AutoVolume
+//
+//  Created by Daniel Luo on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct AutoVolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoVolume/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AutoVolume
+//
+//  Created by Daniel Luo on 12/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
