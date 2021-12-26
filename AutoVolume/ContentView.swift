76f4093849cb8a0d2f0ec7566011ebe3bbdb272f
Daniel Luo <dyl24@case.edu>
2021-12-26 15:35:01 -0500

    Added volume updating and location auth request

--- AutoVolume/ContentView.swift
+++ AutoVolume/ContentView.swift
@@ -6,12 +6,39 @@
 //
 
 import SwiftUI
+import MediaPlayer
+import CoreLocation
 
 struct ContentView: View {
+    
+    let locationManager = CLLocationManager()
+    
+    init() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+    }
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+        Button("Update Volume", action: update)
     }
+    
+    func update() {
+        locationManager.requestWhenInUseAuthorization()
+        
+    }
+    
+    func updateVolume(volume: Float) {
+        let volumeView = MPVolumeView()
+        if let slider = volumeView.subviews.first as? UISlider
+        {
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.01) {
+                slider.value = volume
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
